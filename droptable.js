b2569ef9b8ca7bcf10b0ff8d80fd944999f9a64c
Burkay Ozdemir <hi@burkayozdemir.com>
2018-02-05 01:51:43 +0300

    droptable finished

--- droptable.js
+++ droptable.js
@@ -9,7 +9,7 @@ async function randomPick(items) {
   let currentWeight = 0;
   return items.find(item => {
     currentWeight += item.weight;
-    if (currentWeight > targetWeight) {
+    if (currentWeight >= targetWeight) {
       return item;
     }
   });
@@ -75,50 +75,6 @@ function createItem(name) {
     name: name
   };
 }
-async function Test() {
-  let dropCount = await randomNumber(1, 2);
-  const chestOne = createLootTable('gold chest', dropCount);
-  dropCount = await randomNumber(1, 2);
-  const chestTwo = createLootTable('silver chest', dropCount);
-  dropCount = await randomNumber(1, 2);
-  const megaChest = createLootTable('mega chest', dropCount);
-  dropCount = await randomNumber(1, 2);
-  const equipmentTable = createLootTable('equipments', dropCount);
 
-  let gold = createItem('gold');
-  let silver = createItem('silver');
-  let sword = createItem('sword');
-  let helmet = createItem('helmet');
-
-  equipmentTable.add(sword, 10, false, false, true, 0, 1);
-  equipmentTable.add(helmet, 10, false, false, true, 0, 1);
-
-  chestOne.add(gold, 10, false, true, false, 10, 50);
-  chestOne.add(equipmentTable, 8, false, false, true, 0, 1);
-
-  chestTwo.add(silver, 10, false, true, false, 0, 50);
-  chestTwo.add(equipmentTable, 8, false, false, true, 0, 1);
-
-  megaChest.add(chestOne, 10, true, false, false, 1, 5);
-  megaChest.add(chestTwo, 10, true, false, false, 1, 5);
-
-  let drops = await chestOne.dropLoot();
-  console.log('chest one');
-  drops.forEach(drop => {
-    console.log(drop.item.name + ' ' + drop.stack);
-  });
-  drops = await chestTwo.dropLoot();
-  console.log('chest two');
-  drops.forEach(drop => {
-    console.log(drop.item.name + ' ' + drop.stack);
-  });
-  drops = await megaChest.dropLoot();
-  console.log('mega chest');
-  drops.forEach(drop => {
-    console.log(drop.item.name + ' ' + drop.stack);
-  });
-}
-
-Test().then(() => {
-  console.log('finished');
-});
+module.exports.createItem = createItem;
+module.exports.createLootTable = createLootTable;
